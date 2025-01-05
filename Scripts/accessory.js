5cde715ab8a9fe15ab27db0c845d42136fabeffe
Aastha Lakhanpal <aasthalakhanpal11@gmail.com>
2025-01-05 15:59:58 +0530

    Added: Responsiveness for tablets

--- Scripts/accessory.js
+++ Scripts/accessory.js
@@ -8,18 +8,18 @@ async function accessoryPage() {
     let data = await getData(productUrl).then((arr) => arr);
     let acces = data.filter((el) => el.category === "Bags and Accessories");
     console.log(acces);
-    displayDataInCard(acces, accessCont);
+    displayDataInCard(acces, accessCont, true);
 
     sortByPrice.addEventListener("change", () => {
       accessCont.innerHTML = "";
       if (sortByPrice.value == "htl") {
         acces.sort((a, b) => b.price - a.price);
 
-        displayDataInCard(acces, accessCont);
+        displayDataInCard(acces, accessCont, true);
       } else if (sortByPrice.value == "lth") {
         acces.sort((a, b) => a.price - b.price);
 
-        displayDataInCard(acces, accessCont);
+        displayDataInCard(acces, accessCont, true);
       }
     });
   } catch (error) {
